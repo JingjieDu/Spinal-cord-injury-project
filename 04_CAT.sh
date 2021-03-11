a6af708eb7989838dd2486c91b1dcc5f3fc33455
JingjieDu <60591627+JingjieDu@users.noreply.github.com>
2021-03-11 15:11:07 -0500

    Create 04_CAT.sh

---
+++ 04_CAT.sh
@@ -0,0 +1,4 @@
+module load python/3.5
+mkdir /fs/project/PAS1117/SCI/SCI_ion_all_coassembly/viral_results/CAT_results
+cd /fs/project/PAS1117/SCI/SCI_ion_all_coassembly/viral_results/CAT_results
+/fs/project/PAS1117/bioinformatic_tools/CAT/CAT -d /fs/project/PAS1117/SCI/SCI_ion_all_coassembly/results/04.SCI_ion_all_coassembly.nr.diamond --prefix SCI_ion_all_coassembly --threads 12
