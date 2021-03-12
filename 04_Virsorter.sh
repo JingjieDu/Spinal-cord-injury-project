8b0077254901363acfdb9339263cc07f13d35a46
JingjieDu <60591627+JingjieDu@users.noreply.github.com>
2021-03-11 20:38:02 -0500

    Create 04_Virsorter.sh

---
+++ 04_Virsorter.sh
@@ -0,0 +1,8 @@
+module load bioperl/1.6.1
+module use /fs/project/PAS1117/modulefiles
+module load blast/2.2.26
+module use /users/PAS1117/osu9156/local/share/modulefiles/
+module load virsorter
+
+cd /fs/project/PAS1117/SCI/SCI_ion_all_coassembly/results/
+wrapper_phage_contigs_sorter_iPlant.pl SCI_ion_all_coassembly 01.SCI_ion_all_coassembly.fasta 2 /fs/project/PAS1117/SCI/SCI_ion_all_coassembly/viral_results/ 12
