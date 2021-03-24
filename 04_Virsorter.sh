0df281279b5fd03cb8d287b4e7bf61b1c8d58ffc
JingjieDu <60591627+JingjieDu@users.noreply.github.com>
2021-03-24 12:25:49 -0400

    Update 04_Virsorter.sh

--- 04_Virsorter.sh
+++ 04_Virsorter.sh
@@ -2,8 +2,7 @@ module load bioperl/1.6.1
 module use /fs/project/PAS1117/modulefiles
 module load blast/2.2.26
 module use /users/PAS1117/osu9156/local/share/modulefiles/
+module load virsorter
 
-conda activate virsorter
-export PERL5LIB=/home/jdu7/perl5/lib/perl5/
 cd /fs/project/PAS1117/SCI/SCI_ion_all_coassembly/results/
 wrapper_phage_contigs_sorter_iPlant.pl SCI_ion_all_coassembly 01.SCI_ion_all_coassembly.fasta 2 /fs/project/PAS1117/SCI/SCI_ion_all_coassembly/viral_results/ 12
